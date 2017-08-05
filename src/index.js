fbbdc80a5e36cfeabfcf7b35fbc886e2077648b2
Matt Larson <matt.a.larson@gmail.com>
2017-08-05 10:00:39 -0500

    hello world

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+  </div>,
+  document.getElementById('root')
+);
